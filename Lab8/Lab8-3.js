add334764f3453f2951fac8d59f086c86056994f
Kristina Tommee <ktommee@hawaii.edu>
2019-10-04 11:59:28 -1000

    Create Lab8-3.js

---
+++ Lab8/Lab8-3.js
@@ -0,0 +1,15 @@
+// 3a 
+var age = 20;
+// repeat until counter equals age 
+for(counter = 0; counter <= age; counter++) {
+    console.log ('counter: ' + counter);
+      
+}
+
+// 3b
+var age = 20;
+// repeat until counter equals age 
+for(counter = 50; counter >= age; counter--) {
+    console.log ('counter: ' + counter);
+      
+}
