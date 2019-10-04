93f89179cc84755eb7d32ab70c359686fab28555
Kristina Tommee <ktommee@hawaii.edu>
2019-10-04 11:59:21 -1000

    Create Lab8-1.js

---
+++ Lab8/Lab8-1.js
@@ -0,0 +1,22 @@
+var age = 20;
+var counter = 1; // start counting at 1
+// repeat until counter equals age 
+while(counter != age) {
+    console.log ('counter: ' + counter);
+    counter++;     
+}
+
+//var age = 20;
+//var counter = 1; // start counting at 1
+// repeat until counter equals age 
+//while(counter++ != age) {
+//   console.log ('counter: ' + counter);    
+//}
+
+//var age = 20.5;
+//var counter = 1; // start counting at 1
+// repeat until counter equals age 
+//while(counter <= age) {
+//  console.log ('counter: ' + counter);  
+//    counter++; 
+//}
