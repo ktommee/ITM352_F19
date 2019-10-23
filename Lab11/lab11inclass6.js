a9cac5f81105077accc33d482ffb386ab7524a46
Kristina Tommee <ktommee@hawaii.edu>
2019-10-23 13:12:40 -1000

    Create lab11inclass6.js

---
+++ Lab11/lab11inclass6.js
@@ -0,0 +1,25 @@
+/* for each 
+*/
+
+function isNonNegInt(q, sendArrayBack = false)
+{
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+    if (sendArrayBack)
+    {
+        return (errors);
+    
+    }   else
+    {
+        return(errors.length == 0);
+    }
+    
+}
+
+attributes  =  "Kristina;20;20.5;19.5" ;
+separator = ";";
+pieces = attributes.split(separator);
+pieces.forEach(isNonNegInt);
