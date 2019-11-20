831ed4e7e5149a674c4f3859a60cbc52c85323e2
Kristina Tommee <ktommee@hawaii.edu>
2019-11-20 13:57:36 -1000

    Update lab14ex1.js

--- Lab14/lab14ex1.js
+++ Lab14/lab14ex1.js
@@ -5,4 +5,6 @@ var myParser = require('body-parser');
 var filename = "user_data.json"; // define file name
 var raw_data = fs.readFileSync(filename, 'utf-8');
 var data = JSON.parse(raw_data);
-console.log(data['dport']);
+console.log(data.dport.password);
+
+//console.log(data["dport"]);// log the all the information for dport
