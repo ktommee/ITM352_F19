b3757e09dabf5453365f46b25d5b06c1d552d444
Kristina Tommee <ktommee@hawaii.edu>
2019-11-26 08:28:06 -1000

    Update server.js

--- Tommee_Kristina_Assignment2/server.js
+++ Tommee_Kristina_Assignment2/server.js
@@ -1,5 +1,5 @@
 // Author: Kristina Tommee
-// Description: this is a server to run the product_display.html, index.html and product_invoice.html
+// Description: this is a server to run the product_display.html, index.html and product_invoice.html, login.html, and register.html
 
 // Source: Port Assignment 1 Example + Lab 13 info_server_Ex4.js
 var express = require('express');
@@ -8,8 +8,59 @@ var myParser = require("body-parser");
 var products = require("./public/products.json");
 var querystring = require('querystring');
 var fs = require('fs'); // require readFileSync;
+var filename = "user_registration_info.json"; // define file name
+var quantityqstring;
 
+// only open the file if it exists
+if (fs.existsSync(filename))  // if the file exists
+//read and output the contents
+{
+    var raw_data = fs.readFileSync(filename, 'utf-8');
+    var users_reg_data = JSON.parse(raw_data);
+    console.log(users_reg_data);
+
+
+    fstats = fs.statSync(filename); // data structure that tells you about the file
+    console.log(filename + " has " + fstats.size + " characters "); // tells the size of the file 
+}
+// else tell the user it doesn't exist
+else {
+    console.log('File' + filename + "doesn't exist!");
+}
 app.use(myParser.urlencoded({ extended: true }));
+app.post("/login.html", function (request, response) {
+  // Process login form POST and redirect to logged in page if ok, back to login page if not
+  let POST = request.body; // grab body of request and save it in POST
+  console.log(POST, quantityqstring);
+
+
+  if (typeof POST['submit'] == undefined) {
+      // check if the submit button was pressed.
+      response.redirect("login.html");
+  } else {
+      // user submitted userid and password. test them for validity
+      //check if valid username exists
+      var username = POST.username; // store what was typed in the username textbox in the variable username
+      var usernameLowerCase = username.toLowerCase(); // convert what was typed in the username textbox to all lower case and store in a variable 
+      if (users_reg_data[usernameLowerCase] != undefined) // username exist in user registration data
+      {
+          if (POST.password == users_reg_data[usernameLowerCase].password) // the password correctly corresponds to the defined username in the registration data
+          {
+              response.redirect("product_invoice.html?" + quantityqstring);
+              return; 
+          }
+          else {
+              
+              console.log("Bad password");
+          }
+      }else {
+        console.log("username doesn't exist");
+      }
+response.redirect("login.html");
+  }
+});
+
+
 app.post("/product_display.html", function (request, response) {
   // Code to handle POST from products display
   let POST = request.body;
@@ -32,19 +83,17 @@ app.post("/product_display.html", function (request, response) {
       }
     }
   }
-
+  qstring = querystring.stringify(POST); // post string 
   console.log("quantity=" + total_qty);
   if (has_errors) {
-    qstring = querystring.stringify(POST); // post string 
     response.redirect(`/product_display.html?${qstring}`); // send back to product display page 
   }
   else if (total_qty == 0) { //no quantities chosen
-    qstring = querystring.stringify(POST); // post string 
     response.redirect(`/product_display.html?${qstring}`); // send back to product display page
   }
   else { // redirecting to invoice when quantities are valid
-    qstring = querystring.stringify(POST); // post string
-    response.redirect(`/product_invoice.html?${qstring}`);
+    quantityqstring = qstring; 
+    response.redirect(`/login.html`);
 
   };
 });
@@ -64,77 +113,3 @@ function isNonNegInt(q, returnErrors = false) {
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
 
-
-/* // enable a response
-app.all('*', function (request, response, next) {
-  console.log(request.method + ' to ' + request.path);
-  next();
-});
-
-//process form to POST into invoice
-app.use(myParser.urlencoded({extended: true}));
-app.post("/process_form", function(request,response) {
-  let POST = request.body;
-  var hasValidQuantities = true; // assume all quantities are valid
-  var hasPurchases = false; // assume all quantities are false
-  // loop through products.js array
-  for (i=0; i < products.length; i++) {
-    q = POST ['quantity + i'];
-    if (isNonNegInt (q) == false) {
-      hasValidQuantities = false;
-    }
-    if (q>0) {
-      hasPurchases = true;
-    }
-}
-
-// valid data = invoice
-// unvalid data = error
-qstring = querystring.stringify(POST); // post string
-
-// validates ValidQuantities and hasPurchases
-if(hasValidQuantities == true && hasPurchases == true) {
-  //redirect to invoice page
-  response.redirect("./product_invoice.html" + qString);
-}
-  // if not valid, redirect back to product display
-  else {
-    response.redirect(".product_display.html" + qString);
-  }
-});
-
-
-app.post("./product_display.html", function (request, response) {
-  // Code to handle POST from products display
-  let POST = request.body;
-  POST[`quantity${i}`]
-  // when form is submitted, check if quantities are valid then redirect to invoice
-  if (POST['purchase_submit_button']) {
-    has_errors = false; // assume quantities are valid
-    total_qty = 0; // check if any quantity is selected, checking if total > 0
-    for (i = 0; i < products.length; i++) {
-      if (POST[`quantity${i}`]) {
-        a_qty = params.get(`quantity${i}`);
-        // make textboxes sticky in case of invalid data
-        quantity_form[`quantity${i}`].value = a_qty;
-        total_qty += a_qty;
-        if (!isNonNegInt(a_qty)) {
-          has_errors = true; // if there is invalid quantity
-        }
-      }
-    }
-    //redirect to display page if there is an error or redirect to invoice if values are valid
-    if (has_errors) {
-      qstring = querystring.stringify(POST); // post string
-      response.redirect(`./product_display.html?${qstring}`);
-    } else if (total_qty == 0) { //alert for no quantities chosen
-      qstring = querystring.stringify(POST); // post string
-      response.redirect(`./product_display.html?${qstring}`);
-    } else { // redirecting to invoice when quantities are valid
-      qstring = querystring.stringify(POST); // post string
-      response.redirect(`./product_invoice.html?${qstring}`);
-    }
-  }
-}
-});
-*/
